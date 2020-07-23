2dbc64b3a295f9a1529b64b7747861f92c5d026c
Arnas <1arnasdickus1@gmail.com>
2020-07-23 13:06:13 +0300

    first commit

---
+++ src/app/core/services/form.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+  constructor(private http: HttpClient){}
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+  private sendData(form: NgForm): Promise <any>{
+    return this.http.post('http://127.0.0.1:8000/api/items', JSON.stringify(form.value), {headers: this.headers})
+      .toPromise()
+      .then(res => res || [])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+}
